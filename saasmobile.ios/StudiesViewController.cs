0628d7b18ea292cc76bd770f96c642830c1c0cd6
Adithya Mukund <adithyamukund@msn.com>
2019-01-21 11:00:28 -0500

    Added Registration Form

---
+++ saasmobile.ios/StudiesViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace saasmobile.ios
+{
+    public partial class StudiesViewController : UIViewController
+    {
+        public StudiesViewController() : base("StudiesViewController", null)
+        {
+        }
+
+        protected internal StudiesViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ saasmobile.ios/StudiesViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace saasmobile.ios
+{
+    [Register ("StudiesViewController")]
+    partial class StudiesViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView studiesScreen { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (studiesScreen != null) {
+                studiesScreen.Dispose ();
+                studiesScreen = null;
+            }
+        }
+    }
+}
