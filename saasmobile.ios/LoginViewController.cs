8438dd1185cef51b8e0c68a707dc690319c31576
Adithya Mukund <adithyamukund@msn.com>
2019-01-21 09:53:10 -0500

    Added Crude login feature

---
+++ saasmobile.ios/LoginViewController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using UIKit;
+
+namespace saasmobile.ios
+{
+    public partial class LoginViewController : UIViewController
+    {
+        public LoginViewController() : base("LoginViewController", null)
+        {
+        }
+
+        protected internal LoginViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void LoginButton_TouchUpInside(UIButton sender)
+        {
+            if (isValidEmail() && isValidPassword())
+            {
+                var alert = UIAlertController.Create("Correct Credentials", "Good Job", UIAlertControllerStyle.Alert);
+                ShowViewController(alert, null);
+            }
+            else
+            {
+                var alert = UIAlertController.Create("Incorrect Credentials Entered", "Please try again.", UIAlertControllerStyle.Alert);
+                ShowViewController(alert, null);
+            }
+        }
+
+
+        private bool isValidEmail()
+        {
+            return !String.IsNullOrEmpty(txtEmail.Text.Trim());
+        }
+
+        private bool isValidPassword()
+        {
+            return !String.IsNullOrEmpty(txtPassword.Text.Trim());
+        }
+    }
+}
+
---
+++ saasmobile.ios/LoginViewController.designer.cs
@@ -0,0 +1,51 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace saasmobile.ios
+{
+    [Register ("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtEmail { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPassword { get; set; }
+
+        [Action ("LoginButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void LoginButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (loginButton != null) {
+                loginButton.Dispose ();
+                loginButton = null;
+            }
+
+            if (txtEmail != null) {
+                txtEmail.Dispose ();
+                txtEmail = null;
+            }
+
+            if (txtPassword != null) {
+                txtPassword.Dispose ();
+                txtPassword = null;
+            }
+        }
+    }
+}
