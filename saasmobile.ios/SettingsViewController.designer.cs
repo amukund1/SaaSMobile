aa7ca6be924f90c1c5cffec7d752dddf638ab820
Adithya Mukund <adithyamukund@msn.com>
2019-01-22 21:38:36 -0500

    Added logout feature in settings tab

---
+++ saasmobile.ios/SettingsViewController.designer.cs
@@ -0,0 +1,33 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace saasmobile.ios
+{
+    [Register ("SettingsViewController")]
+    partial class SettingsViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton logoutButton { get; set; }
+
+        [Action ("LogoutButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void LogoutButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (logoutButton != null) {
+                logoutButton.Dispose ();
+                logoutButton = null;
+            }
+        }
+    }
+}
