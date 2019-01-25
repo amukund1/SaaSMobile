36f29e86f1eaa91b09b47e7bf736c9b69df81d60
Tony <tonyqi.2014@gmail.com>
2019-01-24 23:36:30 -0500

    Added Android Register base. Added NuGet package

---
+++ saasmobile.roid/RegisterActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace saasmobile.roid
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your application here
+            SetContentView(Resource.Layout.register_activity);
+        }
+    }
+}
