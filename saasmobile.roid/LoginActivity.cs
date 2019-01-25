b845aca6e301cff2dc9d3f51898117358eee0625
Tony <tonyqi.2014@gmail.com>
2019-01-24 21:24:47 -0500

    Added Android login UI

--- saasmobile.roid/LoginActivity.cs
+++ saasmobile.roid/LoginActivity.cs
@@ -7,13 +7,13 @@ using Android.Widget;
 namespace saasmobile.roid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class LoginActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.login_activity);
         }
     }
 }
